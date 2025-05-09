dc43411d15ebc15b09c61a31b54282a6a485c480
Heather Forester <hforeste07@gmail.com>
2025-05-09 16:30:01 +0000

    working with env

--- app.rb
+++ app.rb
@@ -1,11 +1,10 @@
 require "sinatra"
 require "sinatra/reloader"
 require "http"
-require "dotenv"
 
 get("/") do
   
-  @raw_response = HTTP.get("https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}")
+  @raw_response = HTTP.get("https://api.exchangerate.host/list?access_key=#{env.fetch("EXCHANGE_RATE_KEY").chomp}")
 
   @string_response = @raw_response.to_s
   @parsed_response = JSON.parse(@string_response)
@@ -15,8 +14,25 @@ get("/") do
   erb(:homepage)
 end
 
-get("/:first_symbol") do
+get("/:from_currency") do
+@the_sybol = params.fetch("from_currency")
+
+@url = "https://api.exchangerate.host/list?access_key=#{env.fetch("EXCHANGE_RATE_KEY").chomp}"
+
+@raw_response = HTTP.get(@url)
+@string_response = @raw_response.to_s
+@parsed_response = JSON.parse(@string_response)
+@currencies = @parsed_response.fetch("currencies")
 
   erb(:step_one)
 
+end
+
+get("/:from_currency/:to_currency") do
+@from = params.fetch("from_currency")
+@to = params.fetch("to_currency")
+
+@url = 
+HTTP.get(@url)
+  erb(:step_two)
 end
