2a1b99ea6f88a1f79f2135f6007451cc922b9e1f
Heather Forester <hforeste07@gmail.com>
2025-05-09 19:44:48 +0000

    env fixed

--- app.rb
+++ app.rb
@@ -33,7 +33,8 @@ get("/:from_currency/:to_currency") do
 @from = params.fetch("from_currency")
 @to = params.fetch("to_currency")
 
-@url = 
+@url = "https://api.exchangerate.host/convert?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY").chomp}&from=#{@from}&to=#{@to}&amount=1"
+
 
 
 HTTP.get(@url)
